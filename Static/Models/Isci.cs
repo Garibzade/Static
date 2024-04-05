b22a13a17cb05321ca578f7c2705a3f5c6ebe54f
Garibzade <161576671+Garibzade@users.noreply.github.com>
2024-04-06 03:05:29 +0400

    added static

---
+++ Static/Models/Isci.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static.Models
+{
+    internal class Isci
+    {
+        string _name;
+        string _surname;
+        byte _age;
+        static int  _salary;
+
+        static Isci()
+        {
+            _salary = 3000;
+        }
+        public Isci(string name,string surname,byte age)
+        {
+          
+            _name = name;
+            _surname = surname;
+            _age = age;
+
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine( $"Name:{_name} \n Surename:{_surname}\nAge:{_age} \n maas:{_salary}");
+            
+
+        }
+        public static void BonuseSalary(int bonus)
+        {
+            Console.WriteLine("maas artir");
+            Console.WriteLine("artiqdan soraki maas"+(_salary+bonus));
+        }
+
+        
+
+    }
+}
---
+++ Static/Program.cs
@@ -0,0 +1,15 @@
+﻿using Static.Models;
+
+namespace Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Isci fehle = new Isci("Samir","Kerimov",32);
+            fehle.GetInfo();
+            Isci.BonuseSalary(700);
+
+        }
+    }
+}
